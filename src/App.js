c615d0066fec931b48f9feddf03565c33a5c25ba
kkh061101 <kkh061101@naver.com>
2023-02-07 19:25:05 +0900

    basic webcam test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import './App.scss';
 
 function App() {
-  return <>
-    hello
-  </>;
+  const init = () => {
+    const video = document.querySelector('.Elements');
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log("Something went wrong!");
+        });
+    }
+  }
+  useEffect(() => {
+    init();
+  }, [])
+  return <div className="container">
+    <video autoPlay='true' className="Elements">
+
+    </video>
+  </div>;
 }
 
 export default App;
