35f6ffcb38f95a33d4fa56a41b4a22a67a77b270
kkh061101 <kkh061101@naver.com>
2023-02-16 15:32:15 +0900

    썸네일을 클릭하면 영상으로 이동하게 제작했다

---
+++ src/Thumbnail.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import './Thumbnail.scss';
+
+function Thumbnail() {
+  const [id, setId] = useState([]);
+  useEffect(() => {
+    setId(['h4afwqrOeaw', "6okHyCLorj4"]);
+  }, []);
+  return <div className="thumbnail">
+    {id.map((id, n) =>
+      <div className={`box ${n + 1}`}>
+        <a href={`http://youtu.be/${id}`} target="_blank">
+          <img src={`https://img.youtube.com/vi/${id}/0.jpg`} alt="Thumbnail" />
+        </a>
+      </div>
+    )}
+  </div>;
+}
+
+export default Thumbnail;
