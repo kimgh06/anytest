53fd24c3439df2a676b2bb8f06ebff176ac1ee60
kkh061101 <kkh061101@naver.com>
2023-02-16 15:56:05 +0900

    컴포넌트 개별생성

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -7,29 +7,27 @@ function Thumbnail() {
     setId(['h4afwqrOeaw', "6okHyCLorj4"]);
   }, []);
   return <div className="thumbnail">
-    {id.map((id, n) => {
-      return <Boxes key={n} id={id} n={n} />
-    }
-    )}
+    <h1>Thumbnail test</h1>
+    {id.map((id, n) => <Boxes key={n} id={id} n={n} />)}
   </div>;
 }
 
 function Boxes({ id, n }) {
   const [data, setData] = useState([]);
+  useEffect(() => {
+    parsing();
+  }, []);
   const parsing = async () => {
     const url = `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${id}`;
     const data = await (await fetch(url)).json();
     setData(data);
     console.log(data);
   }
-  useEffect(() => {
-    parsing();
-  }, [])
   return <div className={`box ${n + 1}`}>
     <a href={`http://youtu.be/${id}`} target="_blank" rel="noreferrer">
       <img src={`https://img.youtube.com/vi/${id}/0.jpg`} alt="Thumbnail" />
-      {data.title}
     </a>
+    <b>{data.title}</b>
   </div>;
 }
 
