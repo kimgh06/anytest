fcf24af46123276ad171c1a68732ffde085a23b9
kkh061101 <kkh061101@naver.com>
2023-03-02 08:42:10 +0900

    Box 컴포넌트 분리

--- src/components/Three.js
+++ src/components/Three.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; //3D 모델 불러오기 위한 로더
 import * as THREE from 'three'; //three.js 불러오기
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import './Three.scss';
 
@@ -9,13 +9,28 @@ function Three() {
   return <div className="Three">
     <Canvas id="canvas">
       <OrbitControls autoRotate={true} />
-      <mesh>
-        <ambientLight intensity={1} />
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial attach={'material'} color={'gray'} />
-      </mesh>
+      <Box position={[0, 0, 0]} />
     </Canvas>
   </div>;
+}
+
+function Box(props) {
+  const mesh = useRef(null);
+  useFrame(e => mesh.current.rotation.y = mesh.current.rotation.z += 0.01);
+  const [hovered, setHovered] = useState(false);
+  const [act, setAct] = useState(false);
+  return <mesh
+    ref={mesh}
+    scale={act ? 1.2 : 1}
+    {...props}
+    onPointerOver={e => setHovered(true)}
+    onPointerOut={e => setHovered(false)}
+    onClick={e => setAct(e => !e)}
+  >
+    <ambientLight intensity={1} />
+    <boxGeometry args={[1, 1, 1]} />
+    <meshStandardMaterial attach={'material'} color={hovered ? 'orange' : 'gray'} />
+  </mesh>;
 }
 
 export default Three;
