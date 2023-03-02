7eb7f86f6fbc4300db314d23271724982c0357d0
kkh061101 <kkh061101@naver.com>
2023-03-02 14:26:09 +0900

    Update Three.js

--- src/components/Three.js
+++ src/components/Three.js
@@ -1,35 +1,50 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, Suspense } from "react";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; //3D 모델 불러오기 위한 로더
 import * as THREE from 'three'; //three.js 불러오기
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import './Three.scss';
 
 function Three() {
   return <div className="Three">
     <Canvas id="canvas">
-      <OrbitControls autoRotate={true} />
-      <Box position={[0, 0, 0]} />
+      <Suspense fallback={null}>
+        <OrbitControls autoRotate={true} />
+        <Box position={[0, 0, 0]} />
+        {/* <Model /> */}
+      </Suspense>
     </Canvas>
   </div>;
+}
+
+function Model() {
+  const gltf = useLoader(GLTFLoader, '../3d/scene.gltf');
+  return (
+    <Suspense fallback={null}>
+      <primitive object={gltf.scene} />
+    </Suspense>
+  )
 }
 
 function Box(props) {
   const mesh = useRef(null);
   useFrame(e => mesh.current.rotation.y = mesh.current.rotation.z += 0.01);
+  const [modo, setModo] = useState(false);
   const [hovered, setHovered] = useState(false);
   const [act, setAct] = useState(false);
   return <mesh
     ref={mesh}
-    scale={act ? 1.2 : 1}
+    scale={act ? 1.5 : 1}
     {...props}
     onPointerOver={e => setHovered(true)}
     onPointerOut={e => setHovered(false)}
     onClick={e => setAct(e => !e)}
+    onPointerDown={e => setModo(true)}
+    onPointerUp={e => setModo(false)}
   >
     <ambientLight intensity={1} />
     <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial attach={'material'} color={hovered ? 'orange' : 'gray'} />
+    <meshStandardMaterial attach={'material'} color={modo ? 'hotpink' : hovered ? 'orange' : 'gray'} />
   </mesh>;
 }
 
