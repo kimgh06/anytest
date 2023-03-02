e07af3f38cb27fe13cc41816cd027841c603afef
kkh061101 <kkh061101@naver.com>
2023-03-02 18:12:33 +0900

    정이십면체 추가

--- src/components/Three.js
+++ src/components/Three.js
@@ -1,20 +1,53 @@
 import React, { useState, useRef, Suspense } from "react";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; //3D 모델 불러오기 위한 로더
 import * as THREE from 'three'; //three.js 불러오기
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import './Three.scss';
+import { BufferAttribute, BufferGeometry } from "three";
 
 function Three() {
   return <div className="Three">
-    <Canvas id="canvas">
+    <Canvas id="canvas" camera={{ position: [3, 3, 3] }}>
       <Suspense fallback={null}>
         <OrbitControls autoRotate={false} />
-        <Box position={[0, 0, 0]} />
+        <Box position={[0, 0, 3]} />
+        <Box position={[3, 0, 0]} />
+        <Platform />
+        <EquilateralTriangle />
         <axesHelper args={[5]} />
       </Suspense>
     </Canvas>
   </div>;
+}
+
+function Platform() {
+  const mesh = useRef(null);
+  return <mesh
+    ref={mesh}
+    scale={1}
+    position={[0, -1, 0]}>
+    <boxGeometry args={[15, 1, 10]} />
+    <meshBasicMaterial color={'green'} />
+  </mesh>;
+}
+
+function EquilateralTriangle() {
+  const mesh = useRef(null);
+  useFrame(e => mesh.current.rotation.x = mesh.current.rotation.z += 0.01);
+
+  const radius = 1;
+  const detail = 1;
+
+  const geometry = new THREE.IcosahedronBufferGeometry(radius, detail);
+
+  return (
+    <mesh geometry={geometry}
+      ref={mesh}
+      position={[0, 2, 0]}>
+      <meshBasicMaterial color={"blueviolet"} />
+    </mesh>
+  );
 }
 
 function Box(props) {
@@ -35,7 +68,7 @@ function Box(props) {
   >
     <ambientLight intensity={1} />
     <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial attach={'material'} color={modo ? 'hotpink' : hovered ? 'orange' : 'gray'} />
+    <meshPhysicalMaterial attach={'material'} color={modo ? 'hotpink' : hovered ? 'orange' : 'gray'} />
   </mesh>;
 }
 
