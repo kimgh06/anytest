7a42a75e284a82c35646b3e218f21b261ff5e698
kkh061101 <kkh061101@naver.com>
2023-03-16 22:24:01 +0900

    add express

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 var http = require('http');
 var path = require('path');
+var express = require('express');
 var app = express();
 
 var server = http.createServer(function (request, response) {
-
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('Hello node.js!!');
-
+  // response.writeHead(200, { 'Content-Type': 'text/html' });
+  // response.end('Hello node.js!!');
+  app.use(express.static(path.join(__dirname, 'webcamtest/build')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/webcamtest/build/index.html"));
+  });
 });
 
 server.listen(8080, function () {
