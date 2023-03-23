1b2fe0dbebb84ec09f1305b43fb75256bc492fac
kkh061101 <kkh061101@naver.com>
2023-03-23 17:06:59 +0900

    배포판 추가

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/build'))
 
 app.get('/', (req, res) => {
   res.sendFile('/index.html')
