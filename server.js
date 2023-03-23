39158e4d95da8de96402bfbefe7ed1f99c035f26
kkh061101 <kkh061101@naver.com>
2023-03-23 16:53:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile('/index.html')
 })
 
 app.listen(80, '0.0.0.0', () => {
-  console.log('Server is running : port 8080')
+  console.log('Server is running : port 80')
 })
